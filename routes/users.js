6bb32689c2bf635b9661fd27d631c1e0a94bbd2a
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-25 13:32:49 -0300

    feat: invalid data error

--- routes/users.js
+++ routes/users.js
@@ -1,92 +1,78 @@
 let NeDB = require('nedb');
 let db = new NeDB({
-    filename:'users.db',
+    filename: 'users.db',
     autoload: true
 });
 
 module.exports = (app) => {
 
-    app.get('/users', (req, res) => {
+    let route = app.route('/users');
 
-        db.find({}).sort({name: 1}).exec((err, users)=>{
+    route.get((req, res) => {
 
-            if (err) {
-                console.log(`erroe: ${err}`);
-                res.status(400).json({
-                    error:err
-                });
-            }else {
-        
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users
-        });
-
-    }
-    
-    });
-
-        
-});
-
-    
-    app.post('/users', (req, res) => {
-
-        db.insert(req.body, (err, user) => {
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
 
             if (err) {
-                console.log(`erro: ${err}`);
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req, res);
             } else {
-                
-                res.status(200).json(user);
 
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+                
             }
 
         });
 
     });
 
-    let routeId = app.route(`/users/:id`);
+    route.post((req, res) => {
 
-    routeId.get((req,res)=>{
-    
-        db.findOne({_id:req.params,id}).exec((err, user)=>{
-            
-            if(err) {
+        if (!app.utils.validator.user(app, req, res)) return false;
+ 
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(user);
             }
-        });     
+
+        });
+
     });
-    
-    routeId.put((req,res)=>{
-    
-        db.update({_id:req.params,id}, req.body, err=>{
-            
-            if(err) {
+
+    routeId.put((req, res) => {
+
+        if (!app.utils.validator.user(app, req, res)) return false;
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(Object.assign(req.params, req.body));
             }
-        });     
+
+        });
+
     });
-    
-    routeId.delete((req,res)=>{
 
-        db.remove({_id: req.params.id}, {}, err=>{
+    routeId.delete((req, res) => {
 
-            if(err) {
+        db.remove({ _id: req.params.id }, {}, err => {
+
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(req.params);
             }
+
         });
+
     });
 
 };
