b5bca7cff1f78f4d48e1b17027cf4b994a8a41f0
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-25 10:50:14 -0300

    refactor: consign

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,26 @@
-let express = require('express');
-let routes = express.Router();
+module.exports = function(app){
 
-routes.get('/', (req, res) => {
+    app.get('/users', (req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users:[{
-            name: 'Hcode',
-            email: 'contato@hcode.com.br',
-            id: 1
-        }]
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users:[{
+                name: 'Hcode',
+                email: 'contato@hcode.com.br',
+                id: 1
+            }]
+        });
+    
     });
-
-});
-
-routes.get('/admin', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
+    
+    app.get('/users/admin', (req, res) => {
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: []
+        });
+    
     });
-
-});
-
-module.exports = routes;
+};
