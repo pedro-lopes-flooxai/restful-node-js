e35a19a91a557470485242c054be6889ade6d939
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-25 12:26:01 -0300

    feat: add method for error

--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,29 @@ module.exports = (app) => {
 
     app.get('/users', (req, res) => {
 
+        db.find({}).sort({name: 1}).exec((err, users)=>{
+
+            if (err) {
+                console.log(`erroe: ${err}`);
+                res.status(400).json({
+                    error:err
+                });
+            }else {
+        
+
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json({
-            users:[{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
+            users
         });
+
+    }
     
     });
+
+        
+});
+
     
     app.post('/users', (req, res) => {
 
